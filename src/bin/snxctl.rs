0cf58f1557b992e3781bda5eb6f24e81b11380df
Dmitry Pankratov <dmitry@pankratov.net>
2023-07-28 23:36:38 +0200

    Fixed snxctl usage string

--- src/bin/snxctl.rs
+++ src/bin/snxctl.rs
@@ -43,7 +43,7 @@ async fn main() -> anyhow::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
 
     if args.len() != 2 {
-        return Err(anyhow!("usage: {} {{status|connect|disconnect|reconnect}}", args[0]));
+        return Err(anyhow!("usage: {} {{status|connect|disconnect|reconnect|info}}", args[0]));
     }
 
     let command: SnxCtlCommand = args.get(1).map(AsRef::as_ref).unwrap_or("status").parse()?;
