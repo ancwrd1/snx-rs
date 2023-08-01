e148ff13d4207c8b00fc0006f9f27fa2c3461291
Dmitry Pankratov <dmitry@pankratov.net>
2023-08-02 00:22:19 +0200

    Minor code refactorings

--- src/util.rs
+++ src/util.rs
@@ -5,16 +5,13 @@ use tokio::process::Command;
 use tracing::trace;
 
 // reverse engineered from vendor snx utility
-const TABLE: &[u8] = b"-ODIFIED&W0ROPERTY3HEET7ITH/+4HE3HEET)$3?,$!0?!5?02/0%24)%3.5,,\x10&7?70?/\"*%#43";
+const XOR_TABLE: &[u8] = b"-ODIFIED&W0ROPERTY3HEET7ITH/+4HE3HEET)$3?,$!0?!5?02/0%24)%3.5,,\x10&7?70?/\"*%#43";
 
+#[inline]
 fn translate_byte(i: usize, c: u8) -> u8 {
-    let mut c = if c == 0xff { 0 } else { c };
-    c ^= TABLE[i % 77];
-
-    if c == 0 {
-        0xff
-    } else {
-        c
+    match (c % 255) ^ XOR_TABLE[i % XOR_TABLE.len()] {
+        0 => 255,
+        v => v,
     }
 }
 
