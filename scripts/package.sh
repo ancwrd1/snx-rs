4cc6d1e054ee4c1e28acaa66aba940f26f858a23
Dmitry Pankratov <dmitry@pankratov.net>
2024-08-18 11:35:41 +0200

    Fixes for packaging script

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,20 +3,28 @@
 basedir="$(cd $(dirname $0)/.. && pwd -P)"
 target="$basedir/target"
 version="$(git -C "$basedir" describe)"
-name="snx-rs-$version-linux-x86_64"
+arches="x86_64"
+apps="snx-rs snxctl snx-rs-gui"
+assets="snx-rs.conf snx-rs.service snx-rs-gui.desktop"
 
-rm -rf "$target/$name"
-mkdir "$target/$name"
-if ! cp "$target/x86_64-unknown-linux-gnu/release/snx-rs" "$target/$name/"; then
-    exit 1
-fi
-if ! cp "$target/x86_64-unknown-linux-gnu/release/snxctl" "$target/$name/"; then
-    exit 1
-fi
-if ! cp "$target/x86_64-unknown-linux-gnu/release/snx-rs-gui" "$target/$name/"; then
-    exit 1
-fi
-cp "$basedir/assets/snx-rs.conf" "$basedir/assets/snx-rs.service" "$basedir/assets/snx-rs-gui.desktop" "$target/$name/"
-cd "$target"
-tar cJf "$name.tar.xz" "$name"
-echo "$target/$name.tar.xz"
+for arch in $arches; do
+    name="snx-rs-$version-linux-$arch"
+    triple="$arch-unknown-linux-gnu"
+
+    rm -rf "$target/$name"
+    mkdir "$target/$name"
+
+    for app in $apps; do
+        if ! cp "$target/$triple/release/$app" "$target/$name/"; then
+            exit 1
+        fi
+    done
+
+    for asset in $assets; do
+        cp "$basedir/assets/$asset" "$target/$name/"
+    done
+
+    cd "$target"
+    tar cJf "$name.tar.xz" "$name"
+    echo "$target/$name.tar.xz"
+done
