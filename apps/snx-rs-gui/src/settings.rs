254166ba2e86b2b0d2b44c58a223e5adf4555afb
Dmitry Pankratov <dmitry@pankratov.net>
2026-02-25 21:40:08 +0100

    Bugfix for tunnel type selection UI

--- apps/snx-rs-gui/src/settings.rs
+++ apps/snx-rs-gui/src/settings.rs
@@ -239,8 +239,12 @@ impl MyWidgets {
                 if !is_cert && !self.machine_cert.is_active() {
                     self.cert_type.set_active(Some(0));
                 }
+
                 self.tunnel_type.set_sensitive(!is_mobile_access);
-                self.tunnel_type.set_active(Some(is_mobile_access as _));
+                if is_mobile_access {
+                    self.tunnel_type.set_active_id(Some(TunnelType::Ssl.as_str()));
+                }
+
                 self.machine_cert.set_sensitive(!is_cert && !is_mobile_access);
                 if is_cert || is_mobile_access {
                     self.machine_cert.set_active(false);
